7c05ff1725b3036cfc9d1801b88df9a8982c3606
Kristin Barr <kristinbarr@gmail.com>
2019-03-27 20:33:35 -0400

    completed backend routes

--- server/api/Album.js
+++ server/api/Album.js
@@ -1,17 +1,29 @@
 const router = require('express').Router()
 const Album = require('../db/album')
 const Artist = require('../db/artist')
+const Song = require('../db/song')
 
-router.get('/', async(req, res, next)=>{
-   try {
-       const album = await Album.findAll({
-           include: [{model: Artist}]
-       })
-       res.json(album)
-   } catch (error) {
-       next(error)
-   }
+router.get('/', async (req, res, next) => {
+  try {
+    const album = await Album.findAll({
+      include: [{ model: Artist }]
+    })
+    res.json(album)
+  } catch (error) {
+    next(error)
+  }
 })
 
+router.get('/:albumid', async (req, res, next) => {
+  try {
+    const album = await Album.findOne({
+      where: { id: req.params.albumid },
+      include: [{ model: Artist }, { model: Song }]
+    })
+    res.json(album)
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router
