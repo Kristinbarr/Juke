4cce330fd3273d982c990c38e61ac41413bc6947
blydiet <lydietoussaint123@gmail.com>
2019-03-28 18:48:06 -0400

    added some state

---
+++ albumSeed.js
@@ -0,0 +1,22 @@
+[
+    {
+      "id": 1,
+      "name": "No Dummy",
+      "artworkUrl": "default-album.jpg",
+      "artistId": 1,
+      "artist": {
+        "id": 1,
+        "name": "The Crash Test Dummies"
+      }
+    },
+    {
+      "id": 2,
+      "name": "I React to State",
+      "artworkUrl": "default-album.jpg",
+      "artistId": 1,
+      "artist": {
+        "id": 1,
+        "name": "The Crash Test Dummies"
+      }
+    }
+  ]
--- client/Main.js
+++ client/Main.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import AllAlbums from './AllAlbums'
+import Player from './Player'
+import Sidebar from './Sidebar'
+import albumSeed from '../albumSeed'
 
 export default class Main extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      albums: []
+    }
+  }
   render () {
     return (
       <div id='main' className='row container'>
         {/* The music starts here! */}
+        <Sidebar />
+        <AllAlbums albums = {this.state.albums} />
+        <Player />
       </div>
     )
   }
